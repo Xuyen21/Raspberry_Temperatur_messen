113eb8a926527a647114c4fb692ed3caa0298583
Xuyen <ntxuyen998@gmail.com>
2023-01-18 14:50:31 +0100

    feat: add led to sound detector

--- sound.py
+++ sound.py
@@ -2,18 +2,33 @@
 import RPi.GPIO as GPIO
 import time
 
-#GPIO SETUP
-channel = 22
+# GPIO SETUP
+sound_port = 22
+led_port = 16
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(sound_port, GPIO.IN)
+GPIO.setup(led_port, GPIO.OUT)
+GPIO.setwarnings(False)
+
+
+# led on off
+def led_switch(status):
+    if status:
+        GPIO.output(led_port, GPIO.HIGH)
+    else:
+        GPIO.output(led_port, GPIO.LOW)
+
 
 def callback(channel):
     value = GPIO.input(channel)
     print("callback event %s" % value)
+    led_switch(value)
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on changsy
 
-# infinite loop
+GPIO.add_event_detect(sound_port, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(sound_port, callback)  # assign function to GPIO PIN, Run function on changsy
+
+# run program forever after starting
+# I Use above callbacks to control GPIOs
 while True:
-        time.sleep(1)
+    time.sleep(1)
