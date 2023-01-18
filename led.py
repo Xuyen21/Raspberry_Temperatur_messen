e7b9beb1d18bb07d0cb37ea4225a8d382f2f70f5
Xuyen21 <ntxuten998@gmail.com>
2023-01-18 14:29:25 +0100

    chore: set ports

--- led.py
+++ led.py
@@ -3,9 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(21,GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 print("LED on")
-GPIO.output(21,GPIO.HIGH)
+GPIO.output(16 ,GPIO.HIGH)
 time.sleep(10)
 print("LED off")
-GPIO.output(21,GPIO.LOW)
+GPIO.output(16 ,GPIO.LOW)
--- sound.py
+++ sound.py
@@ -8,13 +8,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
-        if GPIO.input(channel):
-                print "Sound Detected!"
-        else:
-                print "Sound Detected!"
+    value = GPIO.input(channel)
+    print("callback event %s" % value)
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on changsy
 
 # infinite loop
 while True:
